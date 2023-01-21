7fef7f86febac05c95833cb2fd3605956c3bf5ec
Shivaramakrishnan <krish8358@gmail.com>
2023-01-21 22:33:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title("My parents New Healthy Diner")
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly & Vada')
+streamlit.text('with Sambar & Chutney')
+
